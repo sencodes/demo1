000e52614fa911b65ca015b11e6608f02dd6ba0f
sen1 <nrs.senthilkumar@gmail.com>
2016-02-29 01:50:31 +0000

    added a new Java program SampleJava1

---
+++ src/test/java/testpack/SampleJava1.java
@@ -0,0 +1,12 @@
+package testpack;
+
+/**
+ * Created by SenthilKumar on 29/02/2016.
+ */
+public class SampleJava1 {
+
+    public static void main(String[] args){
+
+        System.out.println("Sample Java program");
+    }
+}
